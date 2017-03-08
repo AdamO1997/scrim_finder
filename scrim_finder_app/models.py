5f12232eb57a4137ed619851c49f870e7f1a7450
2191867E <jasonegan59@gmail.com>
2017-03-08 15:41:10 +0000

    new models updates

--- scrim_finder_app/models.py
+++ scrim_finder_app/models.py
@@ -5,9 +5,59 @@ from django.db import models
 
 class userProfile(models.Model):
 
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    email = models.EmailField(max_length = 254, **options)
     picture = models.ImageField(upload_to='profile_images',blank = True)
 
 
     def __str__(self):
         return self.user.username
+
+
+    class Meta:
+
+        ordering = ('user',)
+
+
+class Team(models.Model):
+
+    title = models.CharField(max_length = 30,unique = True)
+    users = models.ManyToManyField(userProfile)
+    full = models.BooleanField(default = False)
+    image = models.ImageField(upload_to='team_images',blank = True)
+
+
+    def _str__(self):
+        return Team.title
+
+    class Meta:
+
+        ordering = ('title',)
+
+
+
+class Games(models.Model):
+    game = models.CharField(max_length = 30, unique = True)
+    usersPlayed = models.ManyToManyField(userProfile)
+    genre = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return Games.game
+
+    Class Meta:
+
+        ordering = ('game',)
+
+
+class Match(models.Model):
+    matchID = models.charField(max_length = 15,unique = True)
+    customName = models.charField(max_length = 20)
+    date = models.DateTimeField()
+    numPlayers = models.IntegerField()
+    game = models.ForeignKey(Games,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return Match.customName
+
+    class Meta:
+        ordering = ('matchID',)

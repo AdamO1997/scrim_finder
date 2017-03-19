31774489922d73af0082d583e1139c6412cb2a66
2191867E <jasonegan59@gmail.com>
2017-03-19 14:52:51 +0000

    models

--- scrim_finder_app/models.py
+++ scrim_finder_app/models.py
@@ -61,6 +61,7 @@ class Match(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     game = models.ForeignKey(Games,on_delete = models.CASCADE)
     teamsPlaying = models.ManyToManyField(Team)
+    full = models.BooleanField(default = False)
 
     def __str__(self):
         return self.matchID
